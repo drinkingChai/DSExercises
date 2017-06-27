81de1e2f083bfa3045bbd613e74308f751ec1f22
Wasif Zaman <teatocode@gmail.com>
2017-06-27 10:02:20 -0400

    Create Linked List

---
+++ LinkedList/linkedlist.js
@@ -0,0 +1,68 @@
+var LNode = {
+  data: null,
+  next: null
+}
+
+var LinkedList = {
+  head: null,
+  add: function(data) {
+    var newNode = Object.create(LNode);
+    newNode.data = data;
+
+    if (this.head === null) {
+      this.head = newNode;
+    } else {
+      var last = this.head;
+      while (last.next !== null) {
+        last = last.next;
+      }
+
+      last.next = newNode;
+    }
+  },
+  addAfter: function(n, data) {
+    var i = 0;
+    var newNode = Object.create(LNode);
+    newNode.data = data;
+
+    var insertAfter = this.head;
+    while (insertAfter.next !== null && i !== n) {
+      insertAfter = insertAfter.next;
+      i++;
+    }
+
+    newNode.next = insertAfter.next;
+    insertAfter.next = newNode;
+  },
+  removeAt: function(n) {
+    if (n == 0) {
+      this.head = this.head.next;
+      return;
+    }
+
+    var i = 0,
+      current = this.head;
+    while (current.next.next !== null && i + 1 !== n) {
+      current = current.next;
+      i++;
+    }
+
+    current.next = current.next.next;
+  },
+  toString: function() {
+    if (this.head === null) {
+      return null;
+    } else {
+      var last = this.head,
+        list = [];
+      while (last.next !== null) {
+        list.push(last.data);
+        last = last.next;
+      }
+
+      list.push(last.data);
+
+      return list;
+    }
+  }
+}
---
+++ LinkedList/test.js
@@ -0,0 +1,5 @@
+var list1 = Object.create(LinkedList);
+
+list1.add("10");
+list1.add("20");
+list1.add("30");
